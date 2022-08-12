22b39b398badec5b22d584b815258a74627e5207
Aloe <heros.dasilva@gmail.com>
2022-08-12 17:14:02 -0300

    [Task: segundo commit ]{12/08/2022 17:12}

---
+++ BlogAPI/Src/Contextos/Contextos.cs
@@ -0,0 +1,26 @@
+﻿
+using BlogAPI.Src.Modelos;
+using Microsoft.EntityFrameworkCore;
+/// <summary>
+/// <para>Resumo: Classe contexto, responsavel por carregar contexto e definir  DbSets </ para >
+/// <para>Criado por: Generation</para>
+/// <para>Versão: 1.0</para>
+/// <para>Data: 17/07/2022</para>
+/// </summary>
+namespace BlogAPI.Src.Contextos
+{
+    public class BlogPessoalContexto : DbContext
+    {
+        #region Atributos
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Tema> Temas { get; set; }
+        public DbSet<Postagem> Postagens { get; set; }
+        #endregion
+        #region Construtores
+        public BlogPessoalContexto(DbContextOptions<BlogPessoalContexto> opt) :
+        base(opt)
+        {
+        }
+        #endregion
+    }
+}
---
+++ BlogAPI/Src/Repositorios/IPostagem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+/// <summary>
+/// <para>Resumo: Responsavel por representar ações de CRUD de postagem</para>
+/// <para>Criado por: Generation</para>
+/// <para>Versão: 1.0</para>
+/// <para>Data: 29/04/2022</para>
+/// </summary>
+public interface IPostagem
+{
+    Task<List<Postagem>> PegarTodasPostagensAsync();
+    Task<Postagem> PegarPostagemPeloIdAsync(int id);
+    Task NovaPostagemAsync(Postagem postagem);
+    Task AtualizarPostagemAsync(Postagem postagem);
+    Task DeletarPostagemAsync(int id);
+}
